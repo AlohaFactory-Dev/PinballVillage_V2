35680c67c1b333d51a81a6ed96b8d8082bcb4a81
aiden <rptdpa879@gmail.com>
2025-08-13 17:40:49 +0900

    [기능] GoldText 순차적으로 오르는 애니메이션 추가

--- Assets/_Scripts/StageUI/GoldText.cs
+++ Assets/_Scripts/StageUI/GoldText.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using TMPro;
 using UniRx;
 using UnityEngine;
@@ -10,6 +11,8 @@ public class GoldText : MonoBehaviour
     private Animator _animator;
     [SerializeField] private TextMeshProUGUI goldText;
     private int _lastGoldValue = 0;
+    private Tween _goldTween;
+    private float _goldTweenDuration = 0.1f; // 애니메이션 지속 시간
 
     public void Init()
     {
@@ -17,17 +20,29 @@ public class GoldText : MonoBehaviour
         _animator = GetComponent<Animator>();
         _goldManager.GoldAmount.Subscribe(UpdateGoldText).AddTo(this);
         _lastGoldValue = _goldManager.GoldAmount.Value;
+
         UpdateGoldText(_goldManager.GoldAmount.Value);
     }
 
     private void UpdateGoldText(int value)
     {
+        int startValue = _lastGoldValue;
+        int endValue = value;
+        _goldTween?.Kill();
+        _goldTween = DOTween.To(() => startValue, x =>
+            {
+                goldText.text = $"{x}";
+                startValue = x;
+            }, endValue, _goldTweenDuration)
+            .SetEase(Ease.OutQuad)
+            .OnComplete(() =>
+            {
+                goldText.text = $"{endValue}";
+                _lastGoldValue = endValue;
+            }).Play();
         if (_lastGoldValue < value)
         {
             _animator.SetTrigger(_actionTrigger);
         }
-
-        goldText.text = $"{value}";
-        _lastGoldValue = value;
     }
 }
